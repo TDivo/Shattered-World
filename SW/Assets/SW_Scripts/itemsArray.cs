374a71574deed44fa836bc56621140d53bd453a8
TDivo <tpdivilly@gmail.com>
2015-09-16 20:37:16 +0100

    Added Items Script
    Added the bones for the weapons segment

---
+++ SW/Assets/SW_Scripts/itemsArray.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class itemsArray : MonoBehaviour 
+{
+	public class Weapon
+	{
+		public string weaponName;
+
+		public int exp;
+		public int damage;
+		public bool poison;
+		public bool bleed;
+		
+		public Weapon (int xp,int dmg,bool poi,bool cut)
+		{
+			exp = xp;
+			damage = dmg;
+			poison = poi;
+			bleed = cut;
+		}
+	}
+	public Weapon currentWep = new Weapon (36,16,false,true);
+
+	void Start () 
+	{
+	
+	}
+
+	void Update () 
+	{
+		//currentWepHealth = currentHealth - currentWep.damage;
+	}
+}
