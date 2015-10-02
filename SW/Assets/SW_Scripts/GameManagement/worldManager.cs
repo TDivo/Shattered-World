f099b025404a652d3c38c2fbe9f9be2b3d005a9e
TDivo <tpdivilly@gmail.com>
2015-10-02 16:34:07 +0100

    More enemy stuff done

---
+++ SW/Assets/SW_Scripts/GameManagement/worldManager.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class worldManager : MonoBehaviour 
+{
+	public GameObject playerCharacter;
+	public float encounterRate = 10;
+	public playerMovement p_Move;
+	
+	void Start () 
+	{
+		playerCharacter = GameObject.Find("Player");
+		p_Move = playerCharacter.GetComponent<playerMovement>(); 
+	}
+
+	void Update () 
+	{
+		if (encounterRate <= 0)
+		{
+			Battle ();
+			encounterRate = (Random.Range(5.0f,12.0f));
+		}
+		if (p_Move.zPoint > 0.0f || p_Move.zPoint < 0.0f)
+		{
+			encounterRate = (encounterRate - 1 * Time.deltaTime);
+		}
+		else if (p_Move.xPoint > 0.0f || p_Move.xPoint < 0.0f)
+		{
+			encounterRate = (encounterRate - 1 * Time.deltaTime);
+		}
+	}
+	void Battle ()
+	{
+		p_Move.zPoint = 0;
+		p_Move.xPoint = 0;
+		p_Move.isBattling = true;
+		EnemyTypes();
+		p_Move.isBattling = false;
+	}
+	void EnemyTypes()
+	{
+		int enemySize = Random.Range(1,3)+1;
+		int counter = enemySize;
+		float nextPosX = 0.0f;
+		float nextPosZ = 5.0f;
+
+		GameObject enemyPrefab = Resources.Load("SW_EnemyPrefabs/testPrefabs/humanMaleSoldier", typeof (GameObject)) as GameObject;
+
+		while (counter>0)
+		{
+			Vector3 ePos = new Vector3 (playerCharacter.transform.position.x + nextPosX,
+			                            playerCharacter.transform.position.y,
+			                            playerCharacter.transform.position.z + nextPosZ);
+			nextPosX = nextPosX + 5.0f;
+			Instantiate(enemyPrefab,ePos, Quaternion.identity);
+			counter--;
+		}
+	}
+}
--- SW/Assets/SW_Scripts/PlayerScripts/playerMovement.cs
+++ SW/Assets/SW_Scripts/PlayerScripts/playerMovement.cs
@@ -3,8 +3,10 @@ using System.Collections;
 
 public class playerMovement : MonoBehaviour 
 {
-	public float xPoint = 1.0f;
-	public float zPoint = 1.0f;
+	public bool isBattling;
+
+	public float xPoint = 0.0f;
+	public float zPoint = 0.0f;
 	public float yPoint = 0.0f;
 	public float currentSpeed = 5.0f;
 	public float currentGravity = -100.0f;
@@ -19,37 +21,41 @@ public class playerMovement : MonoBehaviour
 
 	void Start () 
 	{
+		isBattling = false;
 		tempChar = GameObject.Find ("Character");
 		playerController = GetComponent<CharacterController>(); 
 	}
 
 	void Update () 
 	{
-		Debug.DrawRay(tempChar.transform.position, tempChar.transform.TransformDirection(Vector3.forward), Color.red);
-		if (playerController.isGrounded)
+		if (isBattling == false)
 		{
-			yPoint = transform.position.y;
-			xPoint = CalculateRotation(Input.GetAxis("Horizontal"));
-			zPoint = CalculateRotation(Input.GetAxis("Vertical"));
-			charRotation = new Vector3(xPoint + transform.position.x,yPoint,zPoint + transform.position.z);
-
-			direction = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
-			direction = transform.TransformDirection(direction);
-			direction *= currentSpeed;
-		}
-		direction.y -= currentGravity * Time.deltaTime;
-		playerController.Move(direction * Time.deltaTime);
-		tempChar.transform.LookAt(charRotation);
-		tempChar.transform.eulerAngles = new Vector3 (0.0f,tempChar.transform.eulerAngles.y,0.0f);
-
-		RaycastHit objHit;
-		if (Physics.Raycast(tempChar.transform.position,tempChar.transform.TransformDirection(Vector3.forward), out objHit, 1.0f))
-		{
-			if (Input.GetKeyDown("e"))
+			Debug.DrawRay(tempChar.transform.position, tempChar.transform.TransformDirection(Vector3.forward), Color.red);
+			if (playerController.isGrounded)
 			{
-				if (objHit.collider.tag == "Action")
+				yPoint = transform.position.y;
+				xPoint = CalculateRotation(Input.GetAxis("Horizontal"));
+				zPoint = CalculateRotation(Input.GetAxis("Vertical"));
+				charRotation = new Vector3(xPoint + transform.position.x,yPoint,zPoint + transform.position.z);
+
+				direction = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+				direction = transform.TransformDirection(direction);
+				direction *= currentSpeed;
+			}
+			direction.y -= currentGravity * Time.deltaTime;
+			playerController.Move(direction * Time.deltaTime);
+			tempChar.transform.LookAt(charRotation);
+			tempChar.transform.eulerAngles = new Vector3 (0.0f,tempChar.transform.eulerAngles.y,0.0f);
+
+			RaycastHit objHit;
+			if (Physics.Raycast(tempChar.transform.position,tempChar.transform.TransformDirection(Vector3.forward), out objHit, 1.0f))
+			{
+				if (Input.GetKeyDown("e"))
 				{
-					Debug.Log("Chest Opened");
+					if (objHit.collider.tag == "Action")
+					{
+						Debug.Log("Chest Opened");
+					}
 				}
 			}
 		}
